1c8d08403769ab3e9f199caa08b03555e18a63da
ShiuliKth <shiulimondoll25@gmail.com>
2022-04-13 09:50:41 +0200

    first calculation added

---
+++ index.js
@@ -0,0 +1,39 @@
+let firstCard = 4;
+let secoendCard = 8;
+let sum = firstCard+secoendCard;
+console.log(sum) ;
+
+if (sum <= 20){
+    console.log("Do you want to draw a new card?")
+}
+else if (sum === 21){
+    console.log("Wohoo! you've got black jack!")
+}
+
+else {
+    console.log("you're out of game")
+}
+
+// let age = 12;
+
+// if (age < 21){
+//     console.log("you cannot enter the club")
+// }
+// else{
+//     console.log("welcome to club")
+// }
+
+// let personAge = 99;
+
+// if (personAge<100){
+//     console.log ("you are not eligible")
+// }
+
+// else if (personAge === 100){
+//     console.log ("Here is your Birthday Card from the King")
+// }
+
+// else {
+//     console.log ("Not eligible, you already gotten one")
+// }
+
