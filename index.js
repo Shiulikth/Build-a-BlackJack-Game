71a929c41bb8a7a5e3fc91ba054456b65ad5255a
ShiuliKth <shiulimondoll25@gmail.com>
2022-04-13 11:04:27 +0200

    newcard button add to html and CSS, function add to js file

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-let firstCard = 10;
+let firstCard = 2;
 let secoendCard = 11;
 let sum = firstCard+secoendCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.querySelector("#card-el");
 
 
 function startGame(){
+    cardEl.textContent = "card: "+ firstCard +","+secoendCard;
+    sumEl.textContent = "sum: "+sum;   
 if (sum <= 20){
     message = "Do you want to draw a new card?";
     
+    
 }
 else if (sum === 21){
-    message = "Wohoo! you've got black jack!"
+    message = "you've got black jack!"
     hasBlackJack = true;
+    
+
     
 }
 
 else {
     message = "you're out of game";
     isAlive = false;
+   
 }
   messageEl.textContent = message;
+}
+
+function newCard(){
+    console.log("new card button clicked")
 }
 
 
